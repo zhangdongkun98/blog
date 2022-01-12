76e1006447e7504d63d10011db98d1098bf7d1ba
zhangdongkun98 <zhangdongkun98@gmail.com>
2022-01-12 17:02:11 +0800

    first commit

---
+++ .clear_env.sh
@@ -0,0 +1,8 @@
+find -name *.aux | xargs rm -rf
+find -name *.log | xargs rm -rf
+find -name *.nav | xargs rm -rf
+find -name *.out | xargs rm -rf
+find -name *.pdf | xargs rm -rf
+find -name *.snm | xargs rm -rf
+find -name *.synctex.gz | xargs rm -rf
+find -name *.toc | xargs rm -rf
